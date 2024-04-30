6a2a66ca39e8361f13540d88c9d05e87c9210407
raiyan1999j <62192284+raiyan1999j@users.noreply.github.com>
2024-04-30 22:06:59 +0600

    filter feature added to my items

--- src/Component/MyItem/MyItem.jsx
+++ src/Component/MyItem/MyItem.jsx
@@ -6,6 +6,7 @@ import Swal from "sweetalert2";
 export default function MyItem() {
   const { userData } = useContext(InfoProvider);
   const [info, setInfo] = useState([]);
+  const [filter,setFilter] = useState([]);
 
   const dataRemove = (value) => {
     Swal.fire({
@@ -19,10 +20,10 @@ export default function MyItem() {
     }).then((result) => {
       if (result.isConfirmed) {
         Swal.fire({
-            title: "Deleted!",
-            text: "Your file has been deleted.",
-            icon: "success",
-          });
+          title: "Deleted!",
+          text: "Your file has been deleted.",
+          icon: "success",
+        });
         const copy = [...info];
 
         const updateValue = copy.filter((val, index) => {
@@ -31,14 +32,13 @@ export default function MyItem() {
 
         setInfo(updateValue);
 
-        fetch(`http://localhost:5000/removeItem/${value}`,{
-            method:"Delete",
-            headers:{
-                'accept':'application/json',
-                'content-type':'application/json'
-            },
-        })
-        
+        fetch(`http://localhost:5000/removeItem/${value}`, {
+          method: "Delete",
+          headers: {
+            accept: "application/json",
+            "content-type": "application/json",
+          },
+        });
       }
     });
   };
@@ -62,14 +62,48 @@ export default function MyItem() {
 
     loadData();
   }, []);
+
+  const formHandler=(event)=>{
+    event.preventDefault();
+    const copy = [...info];
+    let redefined ;
+
+    if(event.target.value == "Yes"){
+      redefined = copy.filter((value,index)=>{
+        return value.custom == "Yes"
+      })
+
+      setFilter(redefined)
+    }else if(event.target.value == "No"){
+      redefined = copy.filter((value,index)=>{
+        return value.custom == "No"
+      })
+
+      setFilter(redefined)
+    }else{
+      setFilter(copy);
+    }
+  }
   return (
     <>
       <section className="w-[1200px] mx-auto my-[50px]">
         <div className="w-full grid grid-cols-[20%_80%] gap-x-4">
-          <div></div>
+          <div>
+            <div className="w-full border border-purple-500 rounded-lg">
+              <form onClick={formHandler}>
+                <select className="select select-bordered w-full max-w-xs border border-purple-600">
+                  <option value="">Customization</option>
+                  <option value="Yes">Yes</option>
+                  <option value="No">No</option>
+                  <option value="Clear">Clear</option>
+                </select>
+              </form>
+            </div>
+          </div>
           <div className="w-full grid grid-cols-3 gap-x-4 gap-y-8">
-            {info.map((value, index) => {
-              return (
+            {
+              filter==""?info.map((value,index)=>{
+                return (
                 <ItemsCard
                   key={value._id}
                   data={value}
@@ -78,7 +112,18 @@ export default function MyItem() {
                   }}
                 />
               );
-            })}
+              }):filter.map((value,index)=>{
+                return (
+                <ItemsCard
+                  key={value._id}
+                  data={value}
+                  removeData={(val) => {
+                    dataRemove(val);
+                  }}
+                />
+              );
+              })
+            }
           </div>
         </div>
       </section>
